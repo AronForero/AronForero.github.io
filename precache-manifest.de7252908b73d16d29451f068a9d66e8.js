1df8222ec1fb246e7e962871970940b6e51a4876
Aron Forero <Foreroo965@gmail.com>
2018-12-26 11:02:30 -0500

    New repository to my personal web page

---
+++ precache-manifest.de7252908b73d16d29451f068a9d66e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d42c9335e951f7da768b",
+    "url": "/static/js/main.d42c9335.chunk.js"
+  },
+  {
+    "revision": "64120bb835526fbe5e35",
+    "url": "/static/js/1.64120bb8.chunk.js"
+  },
+  {
+    "revision": "d42c9335e951f7da768b",
+    "url": "/static/css/main.a8b40c7e.chunk.css"
+  },
+  {
+    "revision": "c1d56c2122a6c40aa1cc16791a19d4ae",
+    "url": "/index.html"
+  }
+];
